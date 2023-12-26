31049ad3ae6f2c4c097022de468097f6c3d60a32
SteveShim <steveshim496@gmail.com>
2023-12-26 13:54:20 +0900

    First Commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  nextConfig: {
+    reactStrictMode: true,
+  },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
