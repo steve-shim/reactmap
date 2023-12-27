59d9899b291eaa7a0fef1789a2130eb4b41e8a89
SteveShim <steveshim496@gmail.com>
2023-12-27 22:57:35 +0900

    Add Custom Marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -54,6 +54,31 @@ export default function Map() {
 
         // 마커가 지도 위에 표시되도록 설정합니다
         marker.setMap(map);
+
+        // 마커 커서가 오버되었을 때 마커 위에 표시할 인포윈도우 생성
+        var content = `<div class="infowindow">${store?.upso_nm}</div>`; // 인포윈도우에 표시될 내용
+
+        // 커스텀 오버레이를 생성합니다
+        var customOverlay = new window.kakao.maps.CustomOverlay({
+          position: markerPosition,
+          content: content,
+          xAnchor: 0.6,
+          yAnchor: 0.91,
+        });
+
+        // 마커에 마우스오버 이벤트를 등록합니다
+        window.kakao.maps.event.addListener(marker, "mouseover", function () {
+          // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+          //infowindow.open(map, marker);
+          customOverlay.setMap(map);
+        });
+
+        // 마커에 마우스아웃 이벤트를 등록합니다
+        window.kakao.maps.event.addListener(marker, "mouseout", function () {
+          // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+          //infowindow.close();
+          customOverlay.setMap(null);
+        });
       });
     });
   };
