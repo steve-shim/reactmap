1fe0fb5a8e42ad062ad64f655012ab5e1ee8f046
steve-shim <steveshim496@gmail.com>
2023-12-28 14:11:58 +0900

    Add API and store click eventListener

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,7 @@
 /*global kakao*/
 
 import Script from "next/script";
-import React from "react";
-import * as stores from "@/data/store_data.json";
+import React, { Dispatch, SetStateAction } from "react";
 
 declare global {
   interface Window {
@@ -13,7 +12,11 @@ declare global {
 const DEFAULT_LAT = 37.497625203; // 33.450701
 const DEFAULT_LNG = 127.03088379; // 126.570667
 
-export default function Map() {
+interface MapProps {
+  setMap: Dispatch<SetStateAction<any>>;
+}
+
+export default function Map({ setMap }: MapProps) {
   const loadKakaoMap = () => {
     // kakao map 로드
     window.kakao.maps.load(() => {
@@ -24,62 +27,9 @@ export default function Map() {
       };
       //new kakao.maps.Map(node, options);
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
+      // map 객체를 상위(index.tsx)로 넘긴 다음 해당 map객체들을 가지고 Marker들을 구현
 
-      // 식당 데이터 마커 띄우기
-      stores?.["DATA"]?.map((store) => {
-        var imageSrc = store?.bizcnd_code_nm
-            ? `/images/markers/${store?.bizcnd_code_nm}.png`
-            : "/images/markers/default.png", // 마커이미지의 주소입니다
-          imageSize = new window.kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
-          imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-        var markerImage = new window.kakao.maps.MarkerImage(
-          imageSrc,
-          imageSize,
-          imageOption
-        );
-
-        // 마커가 표시될 위치입니다
-        var markerPosition = new window.kakao.maps.LatLng(
-          store?.y_dnts,
-          store?.x_cnts
-        );
-
-        // 마커를 생성합니다
-        var marker = new window.kakao.maps.Marker({
-          position: markerPosition,
-          image: markerImage, // 마커이미지 설정
-        });
-
-        // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map);
-
-        // 마커 커서가 오버되었을 때 마커 위에 표시할 인포윈도우 생성
-        var content = `<div class="infowindow">${store?.upso_nm}</div>`; // 인포윈도우에 표시될 내용
-
-        // 커스텀 오버레이를 생성합니다
-        var customOverlay = new window.kakao.maps.CustomOverlay({
-          position: markerPosition,
-          content: content,
-          xAnchor: 0.6,
-          yAnchor: 0.91,
-        });
-
-        // 마커에 마우스오버 이벤트를 등록합니다
-        window.kakao.maps.event.addListener(marker, "mouseover", function () {
-          // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-          //infowindow.open(map, marker);
-          customOverlay.setMap(map);
-        });
-
-        // 마커에 마우스아웃 이벤트를 등록합니다
-        window.kakao.maps.event.addListener(marker, "mouseout", function () {
-          // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-          //infowindow.close();
-          customOverlay.setMap(null);
-        });
-      });
+      setMap(map);
     });
   };
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,37 @@
-import Map from "@/components/Map";
+import { useState, useEffect } from "react";
 
-export default function Home() {
-  return <Map />;
+import Map from "@/components/Map";
+import Markers from "@/components/Markers";
+
+//import * as stores from "@/data/store_data.json";
+import StoreBox from "@/components/StoreBox";
+import { StoreType } from "@/interface";
+
+export default function Home({ stores }: { stores: StoreType[] }) {
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  //const storeDatas = stores["DATA"];
+
+  //console.log("currentStore", currentStore);
+  return (
+    <>
+      {/* map 객체를 상위(index.tsx)로 넘긴 다음 해당 map객체들을 가지고 Marker들을 구현 */}
+      <Map setMap={setMap} />
+      <Markers stores={stores} map={map} setCurrentStore={setCurrentStore} />
+      {/* 현재 클릭한 Store의 정보를 넘겨줄수 있고 x를 눌렀을 때 currentStore의 값을 null로 변경 가능*/}
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
+    </>
+  );
 }
 
-// <h1 className="font-bold">Map Index Page</h1>
-//       <ul className="px-4 py-2">
-//         <li>
-//           <Link href="/stores">맛집 목록</Link>
-//         </li>
-//         <li>
-//           <Link href="/stores/new">맛집 생성</Link>
-//         </li>
-//         <li>
-//           <Link href="/stores/1">맛집 상세 페이지</Link>
-//         </li>
-//         <li>
-//           <Link href="/stores/1/edit">맛집 수정 페이지</Link>
-//         </li>
-//         <li>
-//           <Link href="/users/login">로그인 페이지</Link>
-//         </li>
-//         <li>
-//           <Link href="/users/mypage">마이 페이지</Link>
-//         </li>
-//         <li>
-//           <Link href="/users/likes">찜한 맛집</Link>
-//         </li>
-//       </ul>
+// API를 만든경우 fetch를 할 수 있다
+export async function getStaticProps() {
+  const stores = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  ).then((res) => res.json());
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
+}
